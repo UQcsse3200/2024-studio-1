807c432adea4d8d24f28e0de2151572def5af650
Cameron Badman <94279788+CameronBadman@users.noreply.github.com>
2024-08-12 14:36:09 +1000

    added NumberGenerator

---
+++ source/core/src/main/com/csse3200/game/areas/NumberGenerator.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    private Random random;
+
+    public RandomNumberGenerator(String seed) {
+        // Convert string seed to a long value
+        long seedLong = stringToSeed(seed);
+        this.random = new Random(seedLong);
+    }
+
+    // Method to set seed
+    public void setSeed(String seed) {
+        long seedLong = stringToSeed(seed);
+        this.random = new Random(seedLong);
+    }
+
+    private long stringToSeed(String seed) {
+        // Converts string to a long value
+        return seed.hashCode();
+    }
+
+    public int getRandomInt(int minVal, int maxVal) {
+        // Check minVal greater maxVal
+        if (minVal > maxVal) {
+            throw new IllegalArgumentException("minVal greater than maxVal");
+        }
+        // Generate random integer within a range [minVal, maxVal)
+        return random.nextInt(maxVal - minVal) + minVal;
+    }
+
+    public double getRandomDouble(double minVal, double maxVal) {
+        // Check minVal greater maxVal
+        if (minVal > maxVal) {
+            throw new IllegalArgumentException("minVal greater than maxVal");
+        }
+        // Generate random double within a range [minVal, maxVal)
+        return minVal + (maxVal - minVal) * random.nextDouble();
+    }

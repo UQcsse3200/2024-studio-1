eef5d9f50dbab987549aa4f51039ae881e99452e
dhruvanil <dhruv2605@gmail.com>
2024-08-19 22:10:39 +1000

    Started working on Mapfactory and map loading

--- source/core/src/main/com/csse3200/game/entities/factories/MapFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/MapFactory.java
@@ -1,2 +1,7 @@
-package com.csse3200.game.entities.factories;public class MapFactory {
+package com.csse3200.game.entities.factories;
+
+import com.csse3200.game.files.FileLoader;
+
+public class MapFactory {
+    private static final MapConfigs data = FileLoader.readClass(MapConfig.class)
 }

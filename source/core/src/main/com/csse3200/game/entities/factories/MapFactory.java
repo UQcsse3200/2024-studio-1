316f4a85daa2e147a7a02044e046ba7c8b3179b6
dhruvanil <dhruv2605@gmail.com>
2024-08-19 22:40:37 +1000

    Started working on map loader and implemented json file loading and storing into a variable, need to extract data using methods later.

--- source/core/src/main/com/csse3200/game/entities/factories/MapFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/MapFactory.java
@@ -2,6 +2,16 @@ package com.csse3200.game.entities.factories;
 
 import com.csse3200.game.files.FileLoader;
 
+import java.io.File;
+
 public class MapFactory {
-    private static final MapConfigs data = FileLoader.readClass(MapConfig.class)
+    private static final MapConfigs mapData;
+
+    public static void loadMap (String filePath) {
+        mapData = FileLoader.readClass(MapConfig.class, filePath);
+        if (mapData == null){
+            throw new IllegalStateException("Failed to load mapData from " + filePath);
+        }
+    }
+
 }

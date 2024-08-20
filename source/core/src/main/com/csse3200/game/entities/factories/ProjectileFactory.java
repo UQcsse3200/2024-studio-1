82cd78ca1cb0998fe31ae6ee1dec3fb56f0523a0
brose7561 <ben.rose.7561@gmail.com>
2024-08-20 10:12:32 +1000

    made projectile factory. unlinked to projectileAttackComponent.

--- source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
@@ -1,27 +1,33 @@
 package com.csse3200.game.entities.factories;
-
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Fixture;
 import com.csse3200.game.components.CombatStatsComponent;
-import com.csse3200.game.components.Component;
-import com.csse3200.game.components.TouchAttackComponent;
-import com.csse3200.game.components.tasks.MovementTask;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.ProjectileConfig;
-import com.csse3200.game.files.FileLoader;
 import com.csse3200.game.physics.PhysicsUtils;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
 
+/**
+ * For weapon - Design a projectile config for your gun, held in stats arg.
+ *              Every projectile has an owner entity - check out config.
+ *              One projectile is shot once - shooting removes the projectile.
+ *              Projectiles are made listening for a "shootProjectile" with args including owner id.
+ *              Projectile is unregistered after collision.
+ * For map - Consider physics layers if you want to contain a projectile.
+ * For health players - damage is acted by projectileAttackComponent.
+ *
+ */
 public class ProjectileFactory {
 
-    private static final ProjectileConfig stats =
-            FileLoader.readClass(ProjectileConfig.class, "configs/Projecitle.json"); //needs a real file path
 
-
-    public Entity createProjectile() {
+    /**
+     * makes a new Entity with projectile components.
+     *
+     * @param stats - defines a projectile via a config object - can write from .json .
+     * @return Entity projectile
+     */
+    public Entity createProjectile(ProjectileConfig stats) {
 
         Entity projectile =
                 new Entity()
@@ -29,30 +35,21 @@ public class ProjectileFactory {
                         .addComponent(new PhysicsComponent())
                         .addComponent(new ColliderComponent())
                         .addComponent(new HitboxComponent().setLayer(stats.Layer))
-                        .addComponent(new CombatStatsComponent(stats.health, stats.baseAttack))
-                        .addComponent(new TouchAttackComponent(stats.Layer));
+                        .addComponent(new CombatStatsComponent(stats.health, stats.baseAttack));
+        // needs a projectileAttackComponent to be made
 
         PhysicsUtils.setScaledCollider(projectile, stats.scaleX, stats.scaleY);
         projectile.getComponent(ColliderComponent.class).setDensity(1.5f);
         projectile.getComponent(TextureRenderComponent.class).scaleEntity();
 
+
         return projectile;
     }
 
-    private void shootProjectile(Vector2 startPosition, Vector2 direction, Entity projectile) {
-        projectile.getComponent(TouchAttackComponent.class).create();
-        projectile.getEvents().trigger("shootProjectile", startPosition, direction);
-        //projectile.addComponent(MovementTask()) - move the projectile
-        projectile.getEvents().addListener("collisionStart", this::cleanUp);
-    }
-
-
-    // this method occurs following the collision
-    public void cleanUp(Fixture me, Fixture other) {
-        //
-    }
-
 }
+
+
+
 
 
 

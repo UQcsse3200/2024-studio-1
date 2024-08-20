c49e5de9f9d67aa4b6f44de35ecc286a4fcd3be1
Cooper Blackburn <cooper.blackburn@student.uq.edu.au>
2024-08-20 19:26:29 +1000

    Add base door entity function

--- source/core/src/main/com/csse3200/game/entities/factories/DoorFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/DoorFactory.java
@@ -1,2 +1,19 @@
+package com.csse3200.game.entities.factories;
 
+import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.configs.DoorConfig;
+import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.physics.components.ColliderComponent;
+import com.csse3200.game.physics.components.PhysicsComponent;
+import com.csse3200.game.rendering.TextureRenderComponent;
+
+public class DoorFactory {
+
+    public static createBaseDoor() {
+        Entity door =
+                new Entity()
+                        .addComponent(new PhysicsComponent())
+                        .addComponent(new ColliderComonent)
+    }
+}
 

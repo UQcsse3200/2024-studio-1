5dafb2447c7e93d7f6fecd1b564c00773737b91b
Cooper Blackburn <cooper.blackburn@student.uq.edu.au>
2024-08-21 18:14:41 +1000

    Add event listener for collision printing when colliding

--- source/core/src/main/com/csse3200/game/entities/factories/DoorFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/DoorFactory.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.entities.factories;
 
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.DoorConfig;
@@ -11,6 +12,7 @@ import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
 
 public class DoorFactory {
+    static Entity door;
     public static Entity createDoor() {
         // Room variables will be stored once map rooms instantiated
         Entity door = createBaseDoor();
@@ -20,15 +22,18 @@ public class DoorFactory {
 
         door.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
         door.getComponent(TextureRenderComponent.class).scaleEntity();
-        door.scaleHeight(2.5f);
-        PhysicsUtils.setScaledCollider(door, 0.5f, 0.2f);
+        door.scaleHeight(1f); //2.5f
+        PhysicsUtils.setScaledCollider(door, 0.18f, 0.8f); // 0.5f, 0.2f
+        door.getEvents().addListener("collisionStart", (Fixture other, Fixture other1) -> {
+            System.out.println("I just hit something!");
+        });
         return door;
     }
 
     public static Entity createBaseDoor() {
         Entity door =
                 new Entity()
-                        .addComponent(new TextureRenderComponent("images/items/rounded_door.png"))
+                        .addComponent(new TextureRenderComponent("images/rounded_door.png"))
                         .addComponent(new PhysicsComponent())
                         .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
 

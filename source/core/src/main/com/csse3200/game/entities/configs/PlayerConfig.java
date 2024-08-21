e1d630a91ea3f3704001d8d669e664d9f0f80ad7
manyakhosla <manyakhosla05@gmail.com>
2024-08-21 21:54:19 +1000

    updated playerConfig file to store items and health

--- source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
@@ -6,4 +6,7 @@ package com.csse3200.game.entities.configs;
 public class PlayerConfig extends BaseEntityConfig  {
   public int gold = 1;
   public String favouriteColour = "none";
+  public String[] items = new String[0];
+  public int health = 100;
+
 }

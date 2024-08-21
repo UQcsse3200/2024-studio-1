76acf94d595a72c49991961674b73b3522fad1dc
Jas2310 <jasmine.hong@uqconnect.edu.au>
2024-08-22 09:31:26 +1000

    Updated pickup method

--- source/core/src/main/com/csse3200/game/components/player/inventory/EnergyDrink.java
+++ source/core/src/main/com/csse3200/game/components/player/inventory/EnergyDrink.java
@@ -1,6 +1,7 @@
 package com.csse3200.game.components.player.inventory;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.csse3200.game.components.player.PlayerActions;
 import com.csse3200.game.entities.Entity;
 
 public class EnergyDrink extends UsableItem {
@@ -23,10 +24,14 @@ public class EnergyDrink extends UsableItem {
     @Override
     public void pickup(Inventory inventory) {
         super.pickup(inventory);
+
+        // do the real apply
+
     }
 
     @Override
     public void apply(Entity entity) {
+        // do nothing
 
     }
 }

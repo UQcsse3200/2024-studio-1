271acbe81f78da963544b0841848282a622958a5
justarandomnameduh <nqmtien2502@gmail.com>
2024-08-21 12:07:54 +1000

    Add rat attack task (without damage dealing machanism) to rat entity

--- source/core/src/main/com/csse3200/game/entities/factories/NPCFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/NPCFactory.java
@@ -11,10 +11,7 @@ import com.csse3200.game.components.npc.NPCDeathHandler;
 import com.csse3200.game.components.npc.NPCHealthBarComponent;
 import com.csse3200.game.components.npc.RatAnimationController;
 import com.csse3200.game.components.TouchAttackComponent;
-import com.csse3200.game.components.tasks.ChargeTask;
-import com.csse3200.game.components.tasks.ChaseTask;
-import com.csse3200.game.components.tasks.StraightWanderTask;
-import com.csse3200.game.components.tasks.WanderTask;
+import com.csse3200.game.components.tasks.*;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.BaseEntityConfig;
 import com.csse3200.game.entities.configs.GhostKingConfig;
@@ -53,7 +50,8 @@ public class NPCFactory {
     AITaskComponent aiComponent =
             new AITaskComponent()
                     .addTask(new StraightWanderTask(2f))
-                    .addTask(new ChaseTask(target, 10, 2f, 3f, 1.5f));
+                    .addTask(new ChaseTask(target, 10, 2f, 3f, 1.5f))
+                    .addTask(new RatAttackTask(target, 10, 1.5f, 2.5f));
 
     Entity rat = createBaseNPC(aiComponent);
     BaseEntityConfig config = configs.rat;

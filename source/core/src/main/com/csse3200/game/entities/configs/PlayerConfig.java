6cd7afbe1e0dd2c336ac17614a3497c8b49d7c48
manyakhosla <manyakhosla05@gmail.com>
2024-08-21 20:31:28 +1000

    Revert "Created a new class to manage saving character's state"
    This reverts commit 3ce2bc9ffe724ffc67ec44b6cd28e926838b1af3.

--- source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
@@ -1,15 +1,9 @@
 package com.csse3200.game.entities.configs;
 
-import com.badlogic.gdx.utils.Array;
-import com.csse3200.game.components.player.inventory.Collectible;
-
 /**
  * Defines the properties stored in player config files to be loaded by the Player Factory.
  */
 public class PlayerConfig extends BaseEntityConfig  {
   public int gold = 1;
   public String favouriteColour = "none";
-  public Array<Collectible> items = new Array<>();
-  public int health = 0;
-
 }

072d52b6233b769c241e958b1205b68d63696ef1
Cooper Blackburn <cooper.blackburn@student.uq.edu.au>
2024-08-21 10:32:50 +1000

    Set physics and colliding components for createDoor

--- source/core/src/main/com/csse3200/game/entities/factories/DoorFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/DoorFactory.java
@@ -1,29 +1,37 @@
 package com.csse3200.game.entities.factories;
 
-import com.csse3200.game.areas.Room;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.DoorConfig;
 import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.physics.PhysicsLayer;
+import com.csse3200.game.physics.PhysicsUtils;
 import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
 
 public class DoorFactory {
-    public static Entity createDoor(Room currentRoom, Room nextRoom) {
+    public static Entity createDoor() {
+        // Room variables will be stored once map rooms instantiated
         Entity door = createBaseDoor();
+
 
         // door config can be implemented later if deigned necessary
 
-        door.addComponent(new TextureRenderComponent("images/items/rounded_door.png"));
+        door.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+        door.getComponent(TextureRenderComponent.class).scaleEntity();
+        door.scaleHeight(2.5f);
+        PhysicsUtils.setScaledCollider(door, 0.5f, 0.2f);
         return door;
     }
-
 
     public static Entity createBaseDoor() {
         Entity door =
                 new Entity()
+                        .addComponent(new TextureRenderComponent("images/items/rounded_door.png"))
                         .addComponent(new PhysicsComponent())
-                        .addComponent(new ColliderComponent());
+                        .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+
         return door;
     }
 }

425413b05e9f86b6338f28c6d41feff66f88512c
Sioryn Indianna Nereyeus  Willett <sinwillett@gmail.com>
2024-08-23 18:47:33 +1000

    deleted unnecessary test file

--- source/core/src/test/com/csse3200/game/entities/SavePlayerServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.csse3200.game.entities;
-
-import com.csse3200.game.extensions.GameExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
-
-@ExtendWith(GameExtension.class)
-public class SavePlayerServiceTest {
-
-}

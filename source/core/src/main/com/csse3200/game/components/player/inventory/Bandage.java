6636a61c57370a7a1c493a1342260d0e0073110d
Paras Verma <118629936+Parasverma17@users.noreply.github.com>
2024-08-25 11:56:58 +1000

    Fixing Bandage item class

--- source/core/src/main/com/csse3200/game/components/player/inventory/Bandage.java
+++ source/core/src/main/com/csse3200/game/components/player/inventory/Bandage.java
@@ -2,24 +2,19 @@ package com.csse3200.game.components.player.inventory;
 import com.badlogic.gdx.graphics.Texture;
 import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.entities.Entity;
-
 public class Bandage extends UsableItem{
-
-    private static final int Small_Health_Boost = 25;
+    private static final int Small_Health_Boost = 20;
     @Override
     public void pickup(Inventory inventory) {
         super.pickup(inventory);
     }
-
     @Override
     public void drop(Inventory inventory) {
     }
-
     @Override
     public String getName() {
         return "Bandage";
     }
-
     @Override
     public Texture getIcon() {
         return new Texture("images/items/bandage.png");

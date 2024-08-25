659ee9d060e5f2fbedb23f11f0088d9bc6fd8eb7
Prajwal <jigyasumittal41@gmail.com>
2024-08-25 18:38:07 +0530

    ui theme changed

--- source/core/src/main/com/csse3200/game/GdxGame.java
+++ source/core/src/main/com/csse3200/game/GdxGame.java
@@ -3,7 +3,6 @@ package com.csse3200.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.csse3200.game.options.GameOptions;
 import com.csse3200.game.files.UserSettings;
 import com.csse3200.game.screens.MainGameScreen;
 import com.csse3200.game.screens.MainMenuScreen;
@@ -20,7 +19,6 @@ import static com.badlogic.gdx.Gdx.app;
  */
 public class GdxGame extends Game {
   private static final Logger logger = LoggerFactory.getLogger(GdxGame.class);
-  public GameOptions gameOptions;
 
   @Override
   public void create() {
--- source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/csse3200/game/components/mainmenu/MainMenuDisplay.java
@@ -4,141 +4,116 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.csse3200.game.options.GameOptions;
-import com.csse3200.game.options.GameOptions.Difficulty;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.ui.UIComponent;
+import com.csse3200.game.files.UserSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.EnumMap;
 
 /**
  * A ui component for displaying the Main menu.
  */
 public class MainMenuDisplay extends UIComponent {
-    private static final Logger logger = LoggerFactory.getLogger(MainMenuDisplay.class);
-    private static final float Z_INDEX = 2f;
-    /**
-     * Spacing between UI buttons.
-     */
-    private static final float BTN_SPACING = 15f;
-    private Table table;
-    /**
-     * A nested table that contains the buttons for difficulty selection
-     */
-    private Table diffBtnsTable;
+  private static final Logger logger = LoggerFactory.getLogger(MainMenuDisplay.class);
+  private static final float Z_INDEX = 2f;
+  private Table table;
 
-    @Override
-    public void create() {
-        super.create();
-        addActors();
-    }
+  @Override
+  public void create() {
+    super.create();
+    addActors();
+  }
 
-    private void addActors() {
-        table = new Table();
-        table.setFillParent(true);
-        Image title = new Image(
-                ServiceLocator.getResourceService().getAsset(
-                        "images/box_boy_title.png", Texture.class
-                )
-        );
-        diffBtnsTable = new Table();
+  private void addActors() {
+    UserSettings.Settings settings = UserSettings.get();
 
-        Label startBtn = new Label("Start", skin);
-        EnumMap<Difficulty, TextButton> difficultyBtns = new EnumMap<>(Difficulty.class);
-        for (Difficulty diff : Difficulty.values()) {
-            difficultyBtns.put(diff, new TextButton(diff.toString(), skin, "action"));
-        }
-        TextButton loadBtn = new TextButton("Load", skin);
-        TextButton settingsBtn = new TextButton("Settings", skin);
-        TextButton exitBtn = new TextButton("Exit", skin);
+    table = new Table();
+    table.setFillParent(true);
+    Image title =
+        new Image(
+            ServiceLocator.getResourceService()
+                .getAsset("images/box_boy_title.png", Texture.class));
+    Image bg_logo =
+        new Image(
+            ServiceLocator.getResourceService()
+                .getAsset("images/bg_logo.png", Texture.class));
 
-        // Triggers an event when the button is pressed
-        startBtn.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent changeEvent, Actor actor) {
-                        logger.debug("Start button clicked");
-                        entity.getEvents().trigger("start");
-                    }
-                });
+    TextButton startBtn = new TextButton("Start", skin);
+    TextButton loadBtn = new TextButton("Load", skin);
+    TextButton settingsBtn = new TextButton("Settings", skin);
+    TextButton exitBtn = new TextButton("Exit", skin);
 
-        difficultyBtns.forEach((difficulty, btn) -> btn.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent event, Actor actor) {
-                        GameOptions options = new GameOptions(difficulty);
-                        logger.debug("{} difficulty button clicked", difficulty.toString());
-                        entity.getEvents().trigger("start", options);
-                    }
-                }
-        ));
+    bg_logo.setSize(settings.displayMode.width, settings.displayMode.height);
+    bg_logo.setPosition(0, 0);
 
-        loadBtn.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent changeEvent, Actor actor) {
-                        logger.debug("Load button clicked");
-                        entity.getEvents().trigger("load");
-                    }
-                });
+    // Triggers an event when the button is pressed
+    startBtn.addListener(
+        new ChangeListener() {
+          @Override
+          public void changed(ChangeEvent changeEvent, Actor actor) {
+            logger.debug("Start button clicked");
+            entity.getEvents().trigger("start");
+          }
+        });
 
-        settingsBtn.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent changeEvent, Actor actor) {
-                        logger.debug("Settings button clicked");
-                        entity.getEvents().trigger("settings");
-                    }
-                });
+    loadBtn.addListener(
+        new ChangeListener() {
+          @Override
+          public void changed(ChangeEvent changeEvent, Actor actor) {
+            logger.debug("Load button clicked");
+            entity.getEvents().trigger("load");
+          }
+        });
 
-        exitBtn.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent changeEvent, Actor actor) {
-                        logger.debug("Exit button clicked");
-                        entity.getEvents().trigger("exit");
-                    }
-                });
+    settingsBtn.addListener(
+        new ChangeListener() {
+          @Override
+          public void changed(ChangeEvent changeEvent, Actor actor) {
+            logger.debug("Settings button clicked");
+            entity.getEvents().trigger("settings");
+          }
+        });
 
-        table.add(title);
-        table.row();
-        table.add(startBtn).padTop(BTN_SPACING * 2);
-        table.row();
-        for (TextButton btn : difficultyBtns.values()) {
-            diffBtnsTable.add(btn).spaceLeft(BTN_SPACING).spaceRight(BTN_SPACING);
-        }
-        table.add(diffBtnsTable).padTop(BTN_SPACING);
-        table.row();
-        table.add(loadBtn).padTop(BTN_SPACING);
-        table.row();
-        table.add(settingsBtn).padTop(BTN_SPACING);
-        table.row();
-        table.add(exitBtn).padTop(BTN_SPACING);
+    exitBtn.addListener(
+        new ChangeListener() {
+          @Override
+          public void changed(ChangeEvent changeEvent, Actor actor) {
 
-        stage.addActor(table);
-    }
+            logger.debug("Exit button clicked");
+            entity.getEvents().trigger("exit");
+          }
+        });
 
-    @Override
-    public void draw(SpriteBatch batch) {
-        // draw is handled by the stage
-    }
+    /*table.add(title);
+    table.row();*/
+    table.add(startBtn).padTop(30f);
+    table.row();
+    table.add(loadBtn).padTop(15f);
+    table.row();
+    table.add(settingsBtn).padTop(15f);
+    table.row();
+    table.add(exitBtn).padTop(15f);
 
-    @Override
-    public float getZIndex() {
-        return Z_INDEX;
-    }
+    stage.addActor(bg_logo);
+    stage.addActor(table);
+  }
 
-    @Override
-    public void dispose() {
-        table.clear();
-        diffBtnsTable.clear();
-        super.dispose();
-    }
+  @Override
+  public void draw(SpriteBatch batch) {
+    // draw is handled by the stage
+  }
 
+  @Override
+  public float getZIndex() {
+    return Z_INDEX;
+  }
+
+  @Override
+  public void dispose() {
+    table.clear();
+    super.dispose();
+  }
 }

a03e2c2a9a7a2b61405a2642a11921a9c1998d9b
divyanshddn146 <d.agarwal@uqconnect.edu.au>
2024-08-25 15:23:48 +1000

    Knives Spawn

---
+++ source/core/src/main/com/csse3200/game/components/player/inventory/Knife.java
@@ -0,0 +1,33 @@
+package com.csse3200.game.components.player.inventory;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.csse3200.game.components.player.PlayerActions;
+
+/**
+ * Example Weapon Collectible.
+ */
+public class Knife extends MeleeWeapon {
+
+    @Override
+    public void pickup(Inventory inventory) {
+        super.pickup(inventory);
+    }
+
+    @Override
+    public void drop(Inventory inventory) {
+        super.drop(inventory);
+    }
+
+    @Override
+    public String getName() {
+        return "knife";
+    }
+
+    @Override
+    public Texture getIcon() {
+        return new Texture("knife.png");
+    }
+}
+
+

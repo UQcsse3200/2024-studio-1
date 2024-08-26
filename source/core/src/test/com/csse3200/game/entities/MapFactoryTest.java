98f70ef6f75ab4cf44d36da02045ae0aa160895e
dhruvanil <dhruv2605@gmail.com>
2024-08-26 20:41:03 +1000

    made changes to Map Factory testing file

--- source/core/src/test/com/csse3200/game/entities/MapFactoryTest.java
+++ source/core/src/test/com/csse3200/game/entities/MapFactoryTest.java
@@ -1,43 +1,33 @@
 package com.csse3200.game.entities;
 
-import com.csse3200.game.entities.configs.MapConfigs;
 import com.csse3200.game.entities.factories.MapFactory;
-import com.csse3200.game.extensions.GameExtension;
-import com.csse3200.game.files.FileLoader;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.io.File;
 import java.util.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 //@ExtendWith(GameExtension.class)
-/**public class MapFactoryTest {
-
-    MapConfigs config;
-    HashMap<String, List<String>> roomCons;
-
-    /**MapFactory mapFactory;
+public class MapFactoryTest {
+    Map<String, List<String>> roomConns = new HashMap<>();
+    MapFactory mapFactory = new MapFactory();
     @Before
     public void Setup()
     {
-        config = new MapConfigs();
-        roomCons = new HashMap<>();
         List<String> Conns = new ArrayList<>();
         Conns.add("0_1");
         Conns.add("1_0");
         Conns.add("-1_0");
         Conns.add("0_-1");
+
         List<String> Conns2 = new ArrayList<>();
         Conns2.add("2_0");
         Conns2.add("1_1");
         Conns2.add("0_0");
         Conns2.add("1_-1");
-        roomCons.put("0_1", Conns2);
-        config.room_connections = roomCons;
-        mapFactory = new MapFactory(config);
+        roomConns.put("0_1", Conns2);
+        roomConns.put("0_2", Conns);
     }
 
     @Test
@@ -47,7 +37,7 @@ import static org.junit.Assert.*;
         connects.add("1_0");
         connects.add("-1_0");
         connects.add("0_-1");
-        
-        assertEquals(connects, mapFactory.));
+
+        assertEquals(connects, MapFactory.getRoomConnections("0_0"));
     }
-}**/
+}

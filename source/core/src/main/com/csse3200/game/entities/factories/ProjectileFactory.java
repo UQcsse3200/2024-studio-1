591eeb8d75ade18381ed7c7bc3eb22c269b3579d
Magnius001 <maitronghieu001@gmail.com>
2024-08-27 09:51:48 +1000

    Remove body of createProjectile() for testing weapon

--- source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/ProjectileFactory.java
@@ -20,33 +20,12 @@ import com.csse3200.game.rendering.TextureRenderComponent;
  * NOTE - Entity is unregistered after collision. Add only to a single use entity - dive-bombing bird IDK.
  */
 public class ProjectileFactory {
-
-
     /**
      * Makes a new Entity with projectile components.
      */
     public static Entity createProjectile(ProjectileConfig stats, Vector2 position, Vector2 direction) {
-
-
-
-        Entity projectile =
-                new Entity()
-                        .addComponent(new PhysicsComponent())
-                        .addComponent(new ColliderComponent())
-                        .addComponent(new HitboxComponent().setLayer(stats.Layer))
-                        .addComponent(new CombatStatsComponent(stats.health, stats.baseAttack))
-                        .addComponent(new ProjectileAttackComponent(direction, stats.speed))
-                        .addComponent(new PhysicsMovementComponent())
-                        .addComponent(new TextureRenderComponent(stats.projectileTexturePath));
-
-
-        projectile.setPosition(position);
-        PhysicsUtils.setScaledCollider(projectile, stats.scaleX, stats.scaleY);
-        projectile.getComponent(ColliderComponent.class).setDensity(1.5f);
-        projectile.getComponent(TextureRenderComponent.class).scaleEntity();
-
-
-        return projectile;
+        // Place holder
+        return new Entity();
     }
 
     private ProjectileFactory() {throw new IllegalStateException("Instantiating static util class");}

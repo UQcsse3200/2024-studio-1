627159eb6eff4e327134aecab0eafc0b98c394c0
manyakhosla <manyakhosla05@gmail.com>
2024-08-27 10:33:03 +1000

    fixed some typos

--- source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/PlayerConfig.java
@@ -36,7 +36,7 @@ public class PlayerConfig extends BaseEntityConfig  {
   }
 
   /**
-   * Checks if two players are teh same based on their attributes
+   * Checks if two players are the same based on their attributes
    *
    * @param object The object to compare with the player
    *

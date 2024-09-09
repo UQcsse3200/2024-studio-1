20d03240a250dc22b5ef87ea644b35b6da9a0639
Vy Vu Khanh Ho <116183364+VuKhanhVyHo@users.noreply.github.com>
2024-09-09 13:04:37 +1000

    Clean smell code

--- source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
@@ -1,25 +1,9 @@
 package com.csse3200.game.entities.factories;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.utils.Array;
-import com.csse3200.game.components.CombatStatsComponent;
-import com.csse3200.game.components.player.*;
-import com.csse3200.game.components.player.inventory.*;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.LoadPlayer;
 import com.csse3200.game.entities.configs.PlayerConfig;
 import com.csse3200.game.files.FileLoader;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.PhysicsUtils;
-import com.csse3200.game.physics.components.ColliderComponent;
-import com.csse3200.game.physics.components.HitboxComponent;
-import com.csse3200.game.physics.components.PhysicsComponent;
-import com.csse3200.game.services.ServiceLocator;
-import com.csse3200.game.rendering.AnimationRenderComponent;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 

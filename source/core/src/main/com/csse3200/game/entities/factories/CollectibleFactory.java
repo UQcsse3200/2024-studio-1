34f220fedf051a5bc02d3faa567c15d4d4c34424
Div1o1 <divyanshu@uqconnect.edu.au>
2024-09-10 22:02:13 +1000

    Edited hitboxes for items in the merge branch

--- source/core/src/main/com/csse3200/game/entities/factories/CollectibleFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/CollectibleFactory.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.csse3200.game.components.player.CollectibleComponent;
 import com.csse3200.game.components.player.inventory.*;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.physics.components.CollectibleHitboxComponent;
 import com.csse3200.game.physics.components.HitboxComponent;
 import com.csse3200.game.physics.components.PhysicsComponent;
 import com.csse3200.game.rendering.TextureRenderComponent;
@@ -47,7 +48,7 @@ public class CollectibleFactory extends LoadedFactory {
     public Entity createCollectibleEntity(String specification, Collectible collectible) {
         Entity collectibleEntity = new Entity()
                 .addComponent(new CollectibleComponent(collectible))
-                .addComponent(new HitboxComponent())
+                .addComponent(new CollectibleHitboxComponent())
                 .addComponent(new PhysicsComponent());
 
         Texture texture = collectible.getIcon();

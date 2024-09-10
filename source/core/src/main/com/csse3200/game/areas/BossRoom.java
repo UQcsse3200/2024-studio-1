9cafb9a1165502e5721bdca07c36c9694512ce89
Sioryn Indianna Nereyeus  Willett <sinwillett@gmail.com>
2024-09-10 19:18:48 +1000

    refactored rooms to use abstract methods, for generation.

--- source/core/src/main/com/csse3200/game/areas/BossRoom.java
+++ source/core/src/main/com/csse3200/game/areas/BossRoom.java
@@ -16,26 +16,25 @@ import com.csse3200.game.entities.factories.StairFactory;
  */
 public class BossRoom extends BaseRoom {
 
-    private static final float WALL_THICKNESS = 0.15f;
     @Override
-    protected void initializeSpecifications() {
-        this.animalSpecifications = List.of(
-            //Currently there are three random animals being spawned in base on the level the player is in. Bosses haven't been implemented thus using
-            //currently available animals.
-            List.of("Werewolf"),//boss 1
-            List.of("Minotaur"),//boss 2
-            List.of("Bear")// boss 3
-    );
+    protected List<List<String>> getAnimalSpecifications() {
+        return List.of(
+                //Currently there are three random animals being spawned in base on the level the player is in. Bosses haven't been implemented thus using
+                //currently available animals.
+                List.of("Werewolf"),//boss 1
+                List.of("Minotaur"),//boss 2
+                List.of("Bear")// boss 3
+        );
+    }
 
-
-    this.itemSpecifications = List.of(
-            //List of three lists of items for 3 different levels to be spawned in base on which level player is in.
-            List.of("buff:energydrink", "item:medkit", "melee:knife", "ranged:shotgun", "item:shieldpotion"),
-            List.of("item:bandage", "melee:knife", "ranged:shotgun", "buff:energydrink", "item:shieldpotion"),
-            List.of("ranged:shotgun", "item:medkit", "melee:knife", "item:bandage", "buff:energydrink")
-    );
-    
-    this.isBossRoom = true;
+    @Override
+    protected List<List<String>> getItemSpecifications() {
+        return List.of(
+                //List of three lists of items for 3 different levels to be spawned in base on which level player is in.
+                List.of("buff:energydrink", "item:medkit", "melee:knife", "ranged:shotgun", "item:shieldpotion"),
+                List.of("item:bandage", "melee:knife", "ranged:shotgun", "buff:energydrink", "item:shieldpotion"),
+                List.of("ranged:shotgun", "item:medkit", "melee:knife", "item:bandage", "buff:energydrink")
+        );
     }
 
     /**
@@ -63,14 +62,15 @@ public class BossRoom extends BaseRoom {
         spawnStairs(player, area);
         // }
     }
-    private void spawnStairs (Entity player, MainGameArea area) {
+
+    private void spawnStairs(Entity player, MainGameArea area) {
         Entity stairs = stairFactory.createStair(player.getId());
         GridPoint2 stairPos = stairsPosition(stairs);
     }
 
-    private GridPoint2 stairsPosition (Entity stairs) {
-        int x  = maxGridPoint.x;
-        int y = maxGridPoint.y/2;
-        return new GridPoint2(x,y);
+    private GridPoint2 stairsPosition(Entity stairs) {
+        int x = maxGridPoint.x;
+        int y = maxGridPoint.y / 2;
+        return new GridPoint2(x, y);
     }
 }

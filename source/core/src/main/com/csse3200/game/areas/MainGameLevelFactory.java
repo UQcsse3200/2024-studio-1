cf817e523793da1d7868a600934d3cd94e432899
Cameron Badman <94279788+CameronBadman@users.noreply.github.com>
2024-09-10 11:05:52 +1000

    fixed compilation issues

--- source/core/src/main/com/csse3200/game/areas/MainGameLevelFactory.java
+++ source/core/src/main/com/csse3200/game/areas/MainGameLevelFactory.java
@@ -5,7 +5,7 @@ import com.csse3200.game.entities.Room;
 import com.csse3200.game.entities.factories.CollectibleFactory;
 import com.csse3200.game.entities.factories.NPCFactory;
 import com.csse3200.game.entities.factories.RoomFactory;
-import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.entities.factories.StairFactory;
 
 import java.util.*;
 
@@ -23,7 +23,8 @@ public class MainGameLevelFactory implements LevelFactory {
         RoomFactory roomFactory = new RoomFactory(
                 new NPCFactory(),
                 new CollectibleFactory(),
-                new TerrainFactory(levelNumber)
+                new TerrainFactory(levelNumber),
+                new StairFactory()
         );
         // Sprint 4 Switch the MapGenerator to use Rooms
         Map<String, Room> rooms = new HashMap<>();
@@ -33,11 +34,11 @@ public class MainGameLevelFactory implements LevelFactory {
             int animalIndex = map.mapData.getRoomDetails().get(room_key).get("animal_index");
             rooms.put(room_key, roomFactory.createRoom(
                     map.mapData.getPositions().get(room_key),
-                    "0,0,14,10," + itemIndex + "," + animalIndex));
+                    "0,0,14,10," + animalIndex + "," + itemIndex));
         }
         //creating and adding a boss room instance into the Map containing the rooms for
         // the level
-        rooms.put("BOSS", roomFactory.createBossRoom(List.of("", "", "", ""),
+        rooms.put("BOSS", roomFactory.createBossRoom(List.of("", "", "", "", ""),
                 "0,0,14,10," + levelNumber + "," + levelNumber));
         return new Level(map, levelNumber, rooms);
     }

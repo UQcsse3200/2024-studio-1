7f58b490e97c1f2cdba62128ec8ee5a02c67c28d
manyakhosla <manyakhosla05@gmail.com>
2024-09-11 10:33:38 +1000

    modified palyerfactory

--- source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/csse3200/game/entities/factories/PlayerFactory.java
@@ -1,29 +1,11 @@
 package com.csse3200.game.entities.factories;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.csse3200.game.components.CombatStatsComponent;
-import com.csse3200.game.components.player.*;
-import com.csse3200.game.components.player.inventory.Collectible;
-import com.csse3200.game.components.player.inventory.InventoryComponent;
-import com.csse3200.game.components.player.inventory.ItemPickupComponent;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.entities.LoadPlayer;
 import com.csse3200.game.entities.configs.PlayerConfig;
 import com.csse3200.game.files.FileLoader;
-import com.csse3200.game.physics.PhysicsLayer;
-import com.csse3200.game.physics.PhysicsUtils;
-import com.csse3200.game.physics.components.ColliderComponent;
-//import com.csse3200.game.physics.components.PlayerHitboxComponent;
-import com.csse3200.game.physics.components.HitboxComponent;
-import com.csse3200.game.physics.components.PhysicsComponent;
-import com.csse3200.game.services.ServiceLocator;
-import com.csse3200.game.rendering.AnimationRenderComponent;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -56,59 +38,15 @@ public class PlayerFactory extends LoadedFactory {
      * @return entity
      */
     public Entity createPlayer(){
-        return createPlayer(options.get("default"));
+        LoadPlayer loader = new LoadPlayer();
+        PlayerConfig config = options.get("default");
+        return loader.createPlayer(config);
     }
 
-    /**
-     * Create a player entity
-     *
-     * @param config the config for the player.
-     * @return entity
-     */
-    public Entity createPlayer(PlayerConfig config) {
-        TextureAtlas atlas = new TextureAtlas(config.textureAtlasFilename);
-        TextureRegion defaultTexture = atlas.findRegion("idle");
-
-        InventoryComponent inventoryComponent = new InventoryComponent();
-        Entity player = new Entity()
-                .addComponent(new PlayerConfigComponent(config))
-                .addComponent(new PhysicsComponent())
-                .addComponent(new ColliderComponent())
-                .addComponent(new HitboxComponent().setLayer(PhysicsLayer.PLAYER))
-                .addComponent(new PlayerActions())
-                .addComponent(new CombatStatsComponent(config.health, config.baseAttack, true, 0, 0))
-                .addComponent(inventoryComponent)
-                .addComponent(new ItemPickupComponent())
-                .addComponent(new ShieldComponent())
-                .addComponent(ServiceLocator.getInputService().getInputFactory().createForPlayer())
-                .addComponent(new PlayerStatsDisplay())
-                .addComponent(createAnimationComponent(config.textureAtlasFilename))
-                .addComponent(new PlayerAnimationController())
-                .addComponent(new PlayerInventoryDisplay(inventoryComponent))
-                .addComponent(new PlayerHealthDisplay())
-                .addComponent(new WeaponComponent(
-                        new Sprite(new Texture("images/Weapons/knife.png")),
-                        Collectible.Type.RANGED_WEAPON,
-                        10, 1, 1, 10, 10, 0));
-
-        PhysicsUtils.setScaledCollider(player, 0.6f, 0.3f);
-        player.getComponent(ColliderComponent.class).setDensity(1.5f);
-
-        player.setScale(1f, (float) defaultTexture.getRegionHeight() / defaultTexture.getRegionWidth());
-
-        return player;
-    }
-
-    private AnimationRenderComponent createAnimationComponent(String textureAtlasFilename) {
-        AnimationRenderComponent animator = new AnimationRenderComponent(
-                ServiceLocator.getResourceService().getAsset(textureAtlasFilename,
-                        TextureAtlas.class));
-        animator.addAnimation("idle", 0.2f, Animation.PlayMode.LOOP);
-        animator.addAnimation("walk-left", 0.2f, Animation.PlayMode.LOOP);
-        animator.addAnimation("walk-up", 0.2f, Animation.PlayMode.LOOP);
-        animator.addAnimation("walk-right", 0.2f, Animation.PlayMode.LOOP);
-        animator.addAnimation("walk-down", 0.2f, Animation.PlayMode.LOOP);
-        return animator;
+    public Entity createPlayer(String player) {
+        LoadPlayer loader = new LoadPlayer();
+        PlayerConfig config = options.get(player);
+        return loader.createPlayer(config);
     }
 
     @Override

014eb4aa534a3736d9a3b3a6288ab395eac200bb
Ian <spinto774@yahoo.com>
2024-09-11 12:16:49 +1000

    Update ScreenSwitchCommandTest - nicer imports, added test

--- source/core/src/test/com/csse3200/game/ui/terminal/commands/ScreenSwitchCommandTest.java
+++ source/core/src/test/com/csse3200/game/ui/terminal/commands/ScreenSwitchCommandTest.java
@@ -1,15 +1,16 @@
 package com.csse3200.game.ui.terminal.commands;
 
 import com.csse3200.game.GdxGame;
+import com.csse3200.game.GdxGame.ScreenType;
 import com.csse3200.game.extensions.GameExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
 import static com.csse3200.game.GdxGame.ScreenType.MAIN_MENU;
+import static com.csse3200.game.GdxGame.ScreenType.WIN;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(GameExtension.class)
@@ -27,7 +28,7 @@ class ScreenSwitchCommandTest {
     }
 
     @Test
-    void shouldSwitch() {
+    void switchToMenu() {
         // Should be case-insensitive
         args.add("maiN_meNU");
         command.action(args);
@@ -35,16 +36,24 @@ class ScreenSwitchCommandTest {
     }
 
     @Test
+    void switchToWinScreen() {
+        // Should be case-insensitive
+        args.add("wIn");
+        command.action(args);
+        verify(game).setScreen(WIN);
+    }
+
+    @Test
     void tooManyArgs() {
         args.add("maiN_meNU");
         args.add("extra_invalid_arg");
         command.action(args);
-        verify(game, never()).setScreen(Mockito.any(GdxGame.ScreenType.class));
+        verify(game, never()).setScreen(any(ScreenType.class));
     }
 
     @Test
     void tooFewArgs() {
         command.action(args);
-        verify(game, never()).setScreen(Mockito.any(GdxGame.ScreenType.class));
+        verify(game, never()).setScreen(any(ScreenType.class));
     }
 }

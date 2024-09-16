a0d7391199f4917aefde342fe00ac370bf539bfe
Aryan Somesh Gupta <aryansomesh.gupta@student.uq.edu.au>
2024-09-16 23:23:52 +1000

    Fixed the NPCDeathHandlerTest for the disabled Animation Post-Death and other changes in components

--- source/core/src/test/com/csse3200/game/components/npc/NPCDeathHandlerTest.java
+++ source/core/src/test/com/csse3200/game/components/npc/NPCDeathHandlerTest.java
@@ -2,9 +2,12 @@ package com.csse3200.game.components.npc;
 
 import com.badlogic.gdx.utils.Timer;
 import com.csse3200.game.ai.tasks.AITaskComponent;
+import com.csse3200.game.areas.GameAreaService;
 import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.entities.EntityService;
 import com.csse3200.game.physics.components.PhysicsMovementComponent;
+import com.csse3200.game.physics.components.HitboxComponent;
+import com.csse3200.game.physics.components.ColliderComponent;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.entities.Entity;
@@ -13,15 +16,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(GameExtension.class)
 class NPCDeathHandlerTest {
 
     private Entity entity;
-    private PhysicsMovementComponent physicsMovement;
-    private AITaskComponent aiTaskComponent;
 
     @BeforeEach
     void setUp() {
@@ -29,14 +31,18 @@ class NPCDeathHandlerTest {
         NPCDeathHandler npcDeathHandler = new NPCDeathHandler();
         CombatStatsComponent combatStats = mock(CombatStatsComponent.class);
         AnimationRenderComponent animationRender = mock(AnimationRenderComponent.class);
-        physicsMovement = mock(PhysicsMovementComponent.class);
-        aiTaskComponent = mock(AITaskComponent.class);
+        AITaskComponent aiTaskComponent = mock(AITaskComponent.class);
+        PhysicsMovementComponent physicsMovementComponent = mock(PhysicsMovementComponent.class);
+        HitboxComponent hitboxComponent = mock(HitboxComponent.class);
+        ColliderComponent colliderComponent = mock(ColliderComponent.class);
 
         entity.addComponent(npcDeathHandler)
                 .addComponent(combatStats)
                 .addComponent(animationRender)
-                .addComponent(physicsMovement)
-                .addComponent(aiTaskComponent);
+                .addComponent(aiTaskComponent)
+                .addComponent(physicsMovementComponent)
+                .addComponent(hitboxComponent)
+                .addComponent(colliderComponent);
 
         entity.create();
 
@@ -48,9 +54,15 @@ class NPCDeathHandlerTest {
     void shouldTriggerOnDeathWhenDiedEvent() {
         entity.getEvents().trigger("died");
 
-        // Verify that components were disabled and entity added to deadEntities
-        verify(aiTaskComponent).setEnabled(false);
-        verify(physicsMovement).setEnabled(false);
+        // Fast-forward the Timer to ensure scheduled tasks execute
+        Timer.instance().clear();
+
+        // Verify that relevant components were disabled
+        verify(entity.getComponent(AITaskComponent.class)).setEnabled(false);
+        verify(entity.getComponent(PhysicsMovementComponent.class)).setEnabled(false);
+        verify(entity.getComponent(HitboxComponent.class)).setEnabled(false);
+        verify(entity.getComponent(ColliderComponent.class)).setEnabled(false);
+
         assertTrue(NPCDeathHandler.deadEntities.contains(entity.getId()));
     }
 
@@ -60,9 +72,11 @@ class NPCDeathHandlerTest {
         entity.getEvents().trigger("died");
         entity.getEvents().trigger("died");
 
-        // Verify that the death process is only called once
-        verify(aiTaskComponent, times(1)).setEnabled(false);
-        verify(physicsMovement, times(1)).setEnabled(false);
-        assertEquals(1, NPCDeathHandler.deadEntities.size());
+        // Verify that the components' disabling and death process is only called once
+        verify(entity.getComponent(AITaskComponent.class), times(1)).setEnabled(false);
+        verify(entity.getComponent(PhysicsMovementComponent.class), times(1)).setEnabled(false);
+        verify(entity.getComponent(HitboxComponent.class), times(1)).setEnabled(false);
+        verify(entity.getComponent(ColliderComponent.class), times(1)).setEnabled(false);
+        assertTrue(NPCDeathHandler.deadEntities.contains(entity.getId()));
     }
 }

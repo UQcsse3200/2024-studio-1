7d8fa51fb9dd14ca11c2e31a7b2a7e4078383588
Cooper Blackburn <cooper.blackburn@student.uq.edu.au>
2024-09-24 10:10:35 +1000

    Make room_type determine which room is stored

--- source/core/src/main/com/csse3200/game/areas/MainGameLevelFactory.java
+++ source/core/src/main/com/csse3200/game/areas/MainGameLevelFactory.java
@@ -1,5 +1,6 @@
 package com.csse3200.game.areas;
 
+import com.csse3200.game.areas.Generation.MapGenerator;
 import com.csse3200.game.areas.terrain.TerrainFactory;
 import com.csse3200.game.entities.Room;
 import com.csse3200.game.entities.factories.CollectibleFactory;
@@ -31,14 +32,29 @@ public class MainGameLevelFactory implements LevelFactory {
         for (String room_key : room_keySet) {
             int itemIndex = map.mapData.getRoomDetails().get(room_key).get("item_index");
             int animalIndex = map.mapData.getRoomDetails().get(room_key).get("animal_index");
-            rooms.put(room_key, roomFactory.createRoom(
-                    map.mapData.getPositions().get(room_key),
-                    "0,0,14,10," + animalIndex + "," + itemIndex));
+            int roomType = map.mapData.getRoomDetails().get(room_key).get("room_type");
+            switch (roomType) {
+                case MapGenerator.BOSSROOM:
+                    rooms.put(room_key, roomFactory.createBossRoom(
+                            map.mapData.getPositions().get(room_key),
+                            "0,0,14,10," + levelNumber + "," + levelNumber));
+                    // Not sure whether "boss" or key should be used here
+//                    rooms.put("BOSS", roomFactory.createBossRoom(List.of("", "", "", "", ""),
+//                            "0,0,14,10," + levelNumber + "," + levelNumber));
+                    break;
+                case MapGenerator.NPCROOM:
+                    System.out.print("NPCRoom at " + room_key);
+                    break;
+                case MapGenerator.GAMEROOM:
+                    System.out.print("Gameroom at " + room_key);
+                    break;
+                default:
+                    rooms.put(room_key, roomFactory.createRoom(
+                            map.mapData.getPositions().get(room_key),
+                            "0,0,14,10," + animalIndex + "," + itemIndex));
+                    break;
+            }
         }
-        //creating and adding a boss room instance into the Map containing the rooms for
-        // the level
-        rooms.put("BOSS", roomFactory.createBossRoom(List.of("", "", "", "", ""),
-                "0,0,14,10," + levelNumber + "," + levelNumber));
         return new Level(map, levelNumber, rooms);
     }
 

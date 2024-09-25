8538215dca8c07ccd3f814e1b0dd38905ef1ab78
Sioryn Indianna Nereyeus  Willett <sinwillett@gmail.com>
2024-09-25 14:37:08 +1000

    entity to string is now entities responsibility.
    get names replaced with EntityService's toString Method.
    
    Improved tests.
    
    Names can now be functions.

--- source/core/src/main/com/csse3200/game/entities/EntityService.java
+++ source/core/src/main/com/csse3200/game/entities/EntityService.java
@@ -1,14 +1,13 @@
 package com.csse3200.game.entities;
 
 import com.badlogic.gdx.utils.Array;
-import com.csse3200.game.components.NameComponent;
 import com.csse3200.game.services.ServiceLocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 /**
  * Provides a global access point for entities to register themselves. This allows for iterating
@@ -32,9 +31,11 @@ public class EntityService {
      * @param entity new entity.
      */
     public void register(Entity entity) {
-        logger.debug("Registering {} in entity service", entity);
-        entities.add(entity);
-        entity.create();
+        if (!entities.contains(entity, false)){
+            logger.debug("Registering {} in entity service", entity);
+            entities.add(entity);
+            entity.create();
+        }
     }
 
     /**
@@ -45,7 +46,6 @@ public class EntityService {
     public void unregister(Entity entity) {
         logger.debug("Unregistering {} in entity service", entity);
         entities.removeValue(entity, true);
-
     }
 
     /**
@@ -90,28 +90,10 @@ public class EntityService {
         for (Entity entity : entitiesToRemove) {
             if (!ServiceLocator.getPhysicsService().getPhysics().getWorld().isLocked()){
                 entity.dispose();
-                unregister(entity);
                 removed.add(entity);
             }
         }
         entitiesToRemove.removeAll(removed, true);
-    }
-
-    private String entityToString(Entity entity) {
-        if (entity == null) {
-            return "null";
-        }
-
-        NameComponent name = entity.getComponent(NameComponent.class);
-        if (name != null) {
-            return name.getName();
-        }
-
-        return "Unknown Entity: " + entity;
-    }
-
-    public List<String> getEntityNames() {
-        return Stream.of(getEntities()).map(this::entityToString).toList();
     }
 
     private void createQueuedEntities() {
@@ -136,5 +118,15 @@ public class EntityService {
      */
     public Entity[] getEntities() {
         return entities.toArray(Entity.class);
+    }
+
+    @Override
+    public String toString() {
+        List<String> entries = Arrays.stream(this.getEntities())
+                .map(Entity::toString)
+                .toList();
+        return getEntities().length + " Entities\n" +
+                "ID\tPosition\t\tName\n" +
+                String.join("\n", entries);
     }
 }

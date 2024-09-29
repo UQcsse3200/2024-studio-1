bff90dc100cab55295e6be9600c2f4bb7a70df9c
Liam Morris <l.morris1@student.uq.edu.au>
2024-09-29 21:56:10 +1000

    Added Tombstone png and java file. Changed ring png to increase hitbox around player to decrease entity size

--- source/core/src/main/com/csse3200/game/components/player/inventory/Tombstone.java
+++ source/core/src/main/com/csse3200/game/components/player/inventory/Tombstone.java
@@ -2,11 +2,16 @@ package com.csse3200.game.components.player.inventory;
 
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.GridPoint2;
 import com.csse3200.game.components.CombatStatsComponent;
+import com.csse3200.game.components.player.CollectibleComponent;
 import com.csse3200.game.entities.Entity;
+import com.csse3200.game.services.ServiceLocator;
+import java.util.Random;
 
 
 public class Tombstone extends BuffItem {
+    Random random;
 
     /**
      * Returns the string of the buff item
@@ -34,6 +39,28 @@ public class Tombstone extends BuffItem {
     @Override
     public void effect(Entity entity) {
 
+        GridPoint2 itemEntityPosition = new GridPoint2(xPosition, yPosition);
+        markEntityForRemoval(collisionItemEntity);
+
+        int randomInt = this.random.nextInt(2);
+        Entity newItem = this.randomPetGenerator(randomInt);
+        ServiceLocator.getGameAreaService().getGameArea().spawnEntityAt(newItem, itemEntityPosition, true, true);
+        entity.getEvents().trigger("updateItemsReroll", newItem, collisionItemEntity);
+
+
+    }
+    private Entity randomPetGenerator(int randomNum) {
+        String specification = null;
+        switch (randomNum) {
+            case 0 -> specification = "bat";
+            case 1 -> specification = "bear";
+//                case 2 -> specification = "item:medkit";
+//                case 3 -> specification = "buff:energydrink:High";
+//                case 4 -> specification = "buff:energydrink:Low";
+//                case 5 -> specification = "buff:energydrink:Medium";
+        }
+
+        return collectibleFactory.createCollectibleEntity(specification);
     }
 
     /**

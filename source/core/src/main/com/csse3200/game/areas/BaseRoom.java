8397169f0860e75bf44e3f0d64bdfcebd96f4a74
keagan-stoessel <s4748671@student.uq.edu.au>
2024-09-30 20:29:37 +1000

    Fixed merge issues.

--- source/core/src/main/com/csse3200/game/areas/BaseRoom.java
+++ source/core/src/main/com/csse3200/game/areas/BaseRoom.java
@@ -174,8 +174,7 @@ public abstract class BaseRoom implements Room {
     }
 
     public void removeRoom() {
-        List<String> entityNames = ServiceLocator.getEntityService().getEntityNames();
-        logger.info("Removing room, {} Entities\n{}", entityNames.size(), String.join("\n", entityNames));
+        logger.info("Removing room, {}", ServiceLocator.getEntityService());
 
         for (Entity entity : entities) {
             ServiceLocator.getEntityService().markEntityForRemoval(entity);

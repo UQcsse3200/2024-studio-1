6b5c1d6a46e808b7adc472a5bda454f0d9384c54
Cooper Blackburn <cooper.blackburn@student.uq.edu.au>
2024-10-01 11:07:09 +1000

    Spawn a number of items in randomly positioned

--- source/core/src/main/com/csse3200/game/areas/ShopRoom.java
+++ source/core/src/main/com/csse3200/game/areas/ShopRoom.java
@@ -1,7 +1,9 @@
 
 package com.csse3200.game.areas;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.badlogic.gdx.math.GridPoint2;
 import com.csse3200.game.areas.terrain.TerrainFactory;
@@ -9,18 +11,21 @@ import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.factories.CollectibleFactory;
 import com.csse3200.game.entities.factories.NPCFactory;
 import com.csse3200.game.services.ServiceLocator;
+import com.csse3200.game.utils.RandomNumberGenerator;
 
 public class ShopRoom extends BaseRoom {
+    public RandomNumberGenerator rng;
 
     public ShopRoom(NPCFactory npcFactory, CollectibleFactory collectibleFactory, TerrainFactory terrainFactory,
             List<String> roomConnections, String specification, String roomName) {
                 super(terrainFactory, collectibleFactory, roomConnections, specification, roomName);
+                this.rng = new RandomNumberGenerator(this.getClass().toString());
     }
 
     @Override
     protected List<List<String>> getItemSpecifications() {
         return List.of(
-                List.of("item:medkit:buyable", "buff:heart:buybale"),
+                List.of("item:medkit:buyable", "buff:heart:buybale", "item:medkit:buyable", "item:medkit:buybale"),
                 List.of("item:medkit:buyable", "item:medkit:buybale"),
                 List.of("item:medkit:buyable", "item:medkit:buybale")
         );
@@ -35,9 +40,21 @@ public class ShopRoom extends BaseRoom {
 
     private void spawnItems(MainGameArea area) {
         List<String> itemGroup = this.itemSpecifications.get(this.itemGroup);
-        if (itemGroup != null && itemGroup.size() >= 2) {
-            spawnItem(area, itemGroup.get(0), new GridPoint2(8, 8));
-            spawnItem(area, itemGroup.get(1), new GridPoint2(6, 8));
+        Set<String> usedCoordinates = new HashSet<>();
+        final int MAXGENERATED = 10;
+
+        String coordinate;
+        if (itemGroup != null) {
+            while (usedCoordinates.size() < itemGroup.size() && itemGroup.size() < MAXGENERATED) {
+                int x = rng.getRandomInt(1, 8);
+                int y = rng.getRandomInt(1, 8);
+                coordinate = x + "_" + y;
+                if (!usedCoordinates.contains(coordinate)) {
+                    spawnItem(area, itemGroup.get(usedCoordinates.size()), new GridPoint2(x, y));
+                    usedCoordinates.add(coordinate);
+                }
+            }
+
         }
     }
 

f1581769d3124bf611d233015d1c358941706b17
Phạm Trí Nhân <114942828+TriNhan-Pham@users.noreply.github.com>
2024-10-01 23:03:00 +1000

    Multiple game over screens added

--- source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/csse3200/game/components/CombatStatsComponent.java
@@ -5,7 +5,7 @@ import com.csse3200.game.entities.Entity;
 
 import com.csse3200.game.components.player.ShieldComponent;
 
-//import com.csse3200.game.files.FileLoader;
+import com.csse3200.game.files.FileLoader;
 import com.csse3200.game.rendering.AnimationRenderComponent;
 import com.csse3200.game.services.ServiceLocator;
 import com.csse3200.game.utils.RandomNumberGenerator;
@@ -40,8 +40,8 @@ public class CombatStatsComponent extends Component {
     private final Timer timerFlashSprite;
     private CombatStatsComponent.flashSprite flashTask;
 
-//    private String lastAttackName;
-//    private String filePath = "configs/LastAttack.json";
+    private String lastAttackName;
+    private String filePath = "configs/LastAttack.json";
 
     public CombatStatsComponent(int health, int baseAttack, boolean canBeInvincible, int armor, int buff) {
         this.canBeInvincible = canBeInvincible;
@@ -252,8 +252,15 @@ public class CombatStatsComponent extends Component {
             float damageReduction = armor / (armor + 233.33f); //max damage reduction is 30% based on max armor(100)
             int newHealth = getHealth() - (int) ((attacker.getBaseAttack() + attacker.buff) * (1 - damageReduction));
             setHealth(newHealth);
-//            lastAttackName = attacker.getEntity().getComponent(NameComponent.class).getName();
-//            FileLoader.writeClass(lastAttackName, filePath, FileLoader.Location.LOCAL);
+
+            if (attacker.getEntity() == null || attacker.getEntity().getComponent(NameComponent.class) == null){
+                lastAttackName = "Unknown";
+            }
+            else {
+                lastAttackName = attacker.getEntity().getComponent(NameComponent.class).getName();
+            }
+
+            FileLoader.writeClass(lastAttackName, filePath, FileLoader.Location.LOCAL);
             //ServiceLocator.getResourceService().playSound("sounds/gethit.ogg");
             //ServiceLocator.getResourceService().playSound("sounds/hit2.ogg");
             //ServiceLocator.getResourceService().playSound("sounds/hit3.ogg");
@@ -279,7 +286,7 @@ public class CombatStatsComponent extends Component {
             int newHealth = getHealth() - damage;
             setHealth(newHealth);
             //add animationcontroller
-            if (isDead()) {
+            if (health <= 0) {
                 entity.getEvents().trigger("death");
                 entity.getEvents().trigger("died");
                 entity.getEvents().trigger("checkAnimalsDead");

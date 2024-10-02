0429e5b6a4b84d6d08042314cc624058ae68006f
RAHUL SAGAR PABBA <rahulsagarpabba5@gmail.com>
2024-10-02 12:00:40 +1000

    Pushing updated AOETaskComponent to aoe2

--- source/core/src/main/com/csse3200/game/components/npc/attack/AOEAttackComponent.java
+++ source/core/src/main/com/csse3200/game/components/npc/attack/AOEAttackComponent.java
@@ -5,65 +5,57 @@ import com.badlogic.gdx.math.Vector2;
 import com.csse3200.game.components.CombatStatsComponent;
 import com.csse3200.game.entities.Entity;
 import com.csse3200.game.entities.configs.NPCConfigs;
-import com.csse3200.game.services.ServiceLocator;
-import com.badlogic.gdx.utils.Timer;
-import com.badlogic.gdx.utils.Timer.Task;
 
 /**
- * AOEAttackComponent manages Area of Effect attacks for NPCs, including a preparation phase.
+ * AOEAttackComponent manages Area of Effect attacks for NPCs.
  * This component extends the basic AttackComponent to provide AOE functionality.
  */
 public class AOEAttackComponent extends AttackComponent {
     private final float aoeRadius;
     private Vector2 origin;
-    private final float preparationTime = 2f;
 
     /**
      * Constructs a new AOEAttackComponent.
      *
      * @param target The target entity for the attack.
-     * @param attackRange The maximum range at which the attack can be initiated.
+     * @param attackRange The radius of the area of effect for the attack.
      * @param attackRate The frequency of attacks (attacks per second).
      * @param effectConfigs An array of effect configurations to be applied with the attack.
-     * @param aoeRadius The radius of the area of effect for the attack.
      */
     public AOEAttackComponent(Entity target, float attackRange, float attackRate,
-                              NPCConfigs.NPCConfig.EffectConfig[] effectConfigs, float aoeRadius) {
+                              NPCConfigs.NPCConfig.EffectConfig[] effectConfigs) {
         super(target, attackRange, attackRate, effectConfigs);
-        this.aoeRadius = aoeRadius;
+        this.target = target;
+        this.aoeRadius = attackRange; // Using attackRange as the AOE radius
         this.origin = new Vector2();
+        this.setEnabled(false); // Component starts disabled
     }
 
     /**
-     * Initiates the AOE attack by triggering the preparation phase.
+     * Initiates the AOE attack.
      * This method is called when the attack conditions are met.
      */
     @Override
     public void performAttack() {
-        if (canAttack(entity, target)) {
-            entity.getEvents().trigger("aoe_preparation");
-            logger.info("{} is preparing an AOE attack at {}", entity, origin);
-
-            Timer.schedule(new Task() {
-                @Override
-                public void run() {
-                    executeAOEAttack();
-                }
-            }, preparationTime);
-        }
+        executeAOEAttack();
     }
 
     /**
-     * Executes the actual AOE attack after the preparation phase.
+     * Executes the actual AOE attack.
      * This method applies damage and effects to the target if within the AOE.
      */
-    private void executeAOEAttack() {
+    void executeAOEAttack() {
+        // Trigger the AOE attack event
         entity.getEvents().trigger("aoe_attack");
         logger.info("{} executes AOE attack at {}", entity, origin);
+
+        // Create an AOE circle centered on the entity
         Circle aoeCircle = new Circle(entity.getCenterPosition(), aoeRadius);
 
+        // Check if the target is within the AOE and has combat stats
         if (target.getComponent(CombatStatsComponent.class) != null
                 && aoeCircle.contains(target.getPosition())) {
+            // Apply damage and effects to the target
             logger.info("AOE attack hits {} for {} damage", target, combatStats.getBaseAttack());
             target.getComponent(CombatStatsComponent.class).hit(combatStats);
             applyEffects(target);
@@ -72,6 +64,7 @@ public class AOEAttackComponent extends AttackComponent {
 
     /**
      * Sets the origin point of the AOE attack.
+     * This can be used to adjust the center of the AOE if needed.
      *
      * @param newOrigin The new origin position for the AOE attack.
      */
@@ -81,13 +74,14 @@ public class AOEAttackComponent extends AttackComponent {
 
     /**
      * Determines if the attacker can perform an attack on the target.
+     * In this implementation, it always returns true, allowing attacks at any time.
      *
      * @param attacker The entity performing the attack.
      * @param target The entity being targeted.
-     * @return true if the attack can be performed; false otherwise.
+     * @return Always returns true, indicating the attack can always be performed.
      */
     @Override
     public boolean canAttack(Entity attacker, Entity target) {
-        return target.getPosition().dst(entity.getCenterPosition()) <= aoeRadius;
+        return true;
     }
 }

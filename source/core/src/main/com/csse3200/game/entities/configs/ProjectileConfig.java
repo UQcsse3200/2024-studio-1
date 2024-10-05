7baff13d156afe5f28e220b3cdb34539d071b080
brose7561 <ben.rose.7561@gmail.com>
2024-10-05 21:21:27 +1000

    balance projectile damage.

--- source/core/src/main/com/csse3200/game/entities/configs/ProjectileConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/ProjectileConfig.java
@@ -6,7 +6,7 @@ import com.csse3200.game.physics.PhysicsLayer;
 
 public class ProjectileConfig {
     public int health = 0;
-    public int baseAttack = 50;
+    public int baseAttack = 10;
     public Vector2 speed =  new Vector2(10f, 10f);
     public float scaleX = 0.5f;
     public float scaleY = 0.5f;

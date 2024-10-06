6fe43058fa1805454fed2afd756cfcf1d99caab1
brose7561 <ben.rose.7561@gmail.com>
2024-10-06 11:02:25 +1000

    New projectile scaling. At 0.3f square.

--- source/core/src/main/com/csse3200/game/entities/configs/ProjectileConfig.java
+++ source/core/src/main/com/csse3200/game/entities/configs/ProjectileConfig.java
@@ -8,8 +8,8 @@ public class ProjectileConfig {
     public int health = 0;
     public int baseAttack = 10;
     public Vector2 speed =  new Vector2(10f, 10f);
-    public float scaleX = 0.5f;
-    public float scaleY = 0.5f;
+    public float scaleX = 0.3f;
+    public float scaleY = 0.3f;
     public String projectileAtlasPath = "images/Projectiles/GreenShoot.atlas";
     public short Layer = PhysicsLayer.NPC;
     public float range = 100;

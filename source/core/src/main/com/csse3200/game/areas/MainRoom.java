3e1ff04286c2a3b9df86894e6ae43a68d4f216c3
Varun <vsinh14@gmail.com>
2024-10-12 20:45:32 +1000

    Scaling done for all but dragon which atlas file will be remade

--- source/core/src/main/com/csse3200/game/areas/MainRoom.java
+++ source/core/src/main/com/csse3200/game/areas/MainRoom.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class MainRoom extends EnemyRoom {
     @Override protected List<List<String>> getAnimalSpecifications() {
         return List.of(
-                List.of("Dog", "Snake"),
+                List.of("Rat", "Bear", "Bat", "Dog", "Snake", "Dino", "Minotaur","Dragon"),
                 List.of("Bear", "Snake", "Dino", "Dragon"),
                 List.of("Bear", "Bear", "Minotaur"),
                 List.of("Snake", "Bat", "Minotaur"),

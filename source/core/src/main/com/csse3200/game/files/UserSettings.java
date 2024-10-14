b69b1f5c5e665ad68ebd7bde173aa4c9f2a4a21a
Sahajdeep Singh Narang <sahajdeep3366@gmail.com>
2024-10-14 20:47:00 +0530

    Update UserSettings.java to fix the bugs

--- source/core/src/main/com/csse3200/game/files/UserSettings.java
+++ source/core/src/main/com/csse3200/game/files/UserSettings.java
@@ -99,8 +99,8 @@ public class UserSettings {
         public DisplaySettings displayMode = null;
         public float musicVolume = 0.3f;
         public float soundVolume = 0.5f;
-        public boolean walkWithWASD;
-        public boolean shootWithWASD;
+        public boolean walkWithWASD=true;
+        public boolean shootWithWASD=false;
     }
 
     /**
